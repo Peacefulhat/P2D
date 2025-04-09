9ff6a5fb028ef3249397aa0b66a24c4b661e389b
Peacefulhat <peacefulhat@gmail.com>
2025-04-09 14:30:58 +0530

    some minnor additions

--- main.c
+++ main.c
@@ -1,32 +1,7 @@
-#define PMATH_IMP
 #define ARENA_IMP
 #include "include/arena.h"
-#include <time.h>
-#define REC_POINTS 4
-int main(){
-  Arena *rectangle=alloc_arena();
-  Region* first= alloc_region(4,VEC2);
-  append_region(rectangle, first);
-  
-  vec2 data[REC_POINTS]={
-    {1.0f, 1.0f},
-    {4.0f, 1.0f},
-    {1.0f, 4.0f},
-    {4.0f, 4.0f},
-  };
-  
-  arena_free(rectangle);  
-  
-  
-  
-  
-  printf("Rectangle Shape:\n");
-  
-            vec2 *ptr = (vec2 *)region->mem_region;
 
-  for(int i=0;i<REC_POINTS;i++){
-    printf("%d: {%f,%f}\n",i, (vec2*)first->mem_region[i].x,(vec2*)first->mem_region[i].y);
-  }
-  
+int main(){
+  printf("Hello,P2D!");
   return 0;
 }
